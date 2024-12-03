b216f4fe4233c750ce7f51fb494df777bc8aa16c
Sagnik <sagnikg@login-gpu.cm.cluster>
2024-12-03 01:26:38 -0500

    update shell script to create ablations

--- train_job_fedlora_vit.sh
+++ train_job_fedlora_vit.sh
@@ -7,6 +7,36 @@
 #SBATCH --cpus-per-task 2
 #SBATCH --mem=16G
 #SBATCH --gres=gpu:H100:1
+#SBATCH --array=0-17
+
+#hyperparameters
+client_fractions=(1)
+comm_rounds=(50)
+num_clients=(1 2 5)
+local_learning_rates=(5e-4 1e-4 1e-3)
+alphas=(100 0.1)
+
+total_combinations=$((${#client_fractions[@]} * ${#comm_rounds[@]} * ${#num_clients[@]} * ${#local_learning_rates[@]} * ${#alphas[@]}))
+
+echo "Total combinations: $total_combinations"
+
+# Check if SLURM_ARRAY_TASK_ID is within range
+if [ $SLURM_ARRAY_TASK_ID -ge $total_combinations ]; then
+    echo "Array task ID out of range! SLURM_ARRAY_TASK_ID: $SLURM_ARRAY_TASK_ID"
+    exit 1
+fi
+
+client_fraction_index=$((SLURM_ARRAY_TASK_ID % ${#client_fractions[@]}))
+comm_rounds_index=$(( (SLURM_ARRAY_TASK_ID / ${#client_fractions[@]}) % ${#comm_rounds[@]} ))
+num_clients_index=$(( (SLURM_ARRAY_TASK_ID / (${#client_fractions[@]} * ${#comm_rounds[@]})) % ${#num_clients[@]} ))
+local_learning_rate_index=$(( (SLURM_ARRAY_TASK_ID / (${#client_fractions[@]} * ${#comm_rounds[@]} * ${#num_clients[@]})) % ${#local_learning_rates[@]} ))
+alpha_index=$(( (SLURM_ARRAY_TASK_ID / (${#client_fractions[@]} * ${#comm_rounds[@]} * ${#num_clients[@]} * ${#local_learning_rates[@]})) % ${#alphas[@]} ))
+
+client_fraction=${client_fractions[$client_fraction_index]}
+comm_round=${comm_rounds[$comm_rounds_index]}
+num_client=${num_clients[$lnum_clients_index]}
+local_learning_rate=${local_learning_rates[$local_learning_rate_index]}
+alpha=${alphas[$alpha_index]}
 
 source activate base
 conda activate fedlora
@@ -15,5 +45,5 @@ apptainer exec --nv /home/sagnikg/containers/miniconda3_latest.sif bash -c "
     source activate base
     conda activate fedlora
     export SSL_CERT_FILE=\$(python -m certifi)
-    python main.py --num_communication_rounds 10 --num_clients 2 --client_selection_frac 1 --alpha 100
+    python main.py --num_communication_rounds $comm_round --num_clients $num_client --local_learning_rate $local_learning_rate --client_selection_frac $client_fraction --alpha $alpha
 "
