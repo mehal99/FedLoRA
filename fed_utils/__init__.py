944e991c7b066423546a130229b23011774b8f7c
Sagnik <sagnikg@login-gpu.cm.cluster>
2024-12-02 23:48:03 -0500

    initialized modules

--- fed_utils/__init__.py
+++ fed_utils/__init__.py
@@ -3,3 +3,4 @@ from .client_participation_scheduling import client_selection
 from .client import GeneralClient
 from .evaluation import global_evaluation
 from .other import other_function
+from .train_utils import test_batch_cls, eval_loop
